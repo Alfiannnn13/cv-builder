ad69c1e4017af87ba70a959aa0bbf62259ef8d91
Alfiannnn13 <muhammadalfiannovanto17@gmail.com>
2025-01-16 14:08:26 +0700

    pendidikan

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -2,14 +2,24 @@ import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
-
+import EducationForm from "./forms/EducationForm";
 
 export const steps: {
-    title: string;
-    component: React.ComponentType<EditorFormProps>;
-    key: string;
+  title: string;
+  component: React.ComponentType<EditorFormProps>;
+  key: string;
 }[] = [
-    {title: "Info CV", component: GeneralInfoForm, key: "informasi-cv"},
-    {title: "Data Diri", component: PersonalInfoForm, key: "data-diri"},
-    {title: "Pengalaman Kerja", component: WorkExperienceForm, key: "pengalaman-kerja"},
-]
+  { title: "Info CV", component: GeneralInfoForm, key: "informasi-cv" },
+  { title: "Data Diri", component: PersonalInfoForm, key: "data-diri" },
+  {
+    title: "Pendidikan",
+    component: EducationForm,
+    key: "pendidikan",
+  },
+  {
+    title: "Pengalaman Kerja",
+    component: WorkExperienceForm,
+    key: "pengalaman-kerja",
+  },
+  
+];
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -48,10 +48,26 @@ export const workExperienceSchema = z.object({
 
 export type workExperiencesValues = z.infer<typeof workExperienceSchema>
 
+// education
+export const educationSchema = z.object({
+  educations: z.array(
+    z.object({
+      degree: optionalString,
+      school: optionalString,
+      startDate: optionalString,
+      endDate: optionalString,
+    })
+  )
+  .optional(),
+})
+
+export type EducationValues = z.infer<typeof educationSchema>
+
 export const resumeSchema = z.object({
   ...generalInfoSchema.shape,
   ...personalInfoSchema.shape,
   ...workExperienceSchema.shape,
+  ...educationSchema.shape,
 });
 
 export type ResumeValues = Omit<z.infer<typeof resumeSchema>, "photo"> & {
