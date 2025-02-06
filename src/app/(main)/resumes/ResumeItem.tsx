092a0127f5946650deb9cfc138571980329562e7
Alfiannnn13 <muhammadalfiannovanto17@gmail.com>
2025-02-06 13:44:25 +0700

    d

--- src/app/(main)/resumes/ResumeItem.tsx
+++ src/app/(main)/resumes/ResumeItem.tsx
@@ -1,42 +1,173 @@
-"use client"
+"use client";
 
 import ResumePreview from "@/components/ResumePreview";
-import { ResumeServerData } from "@/lib/types"
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useToast } from "@/hooks/use-toast";
+import { ResumeServerData } from "@/lib/types";
 import { mapToResumeValues } from "@/lib/utils";
 import { formatDate } from "date-fns";
+import { MoreVertical, Printer, Trash, Trash2 } from "lucide-react";
 import Link from "next/link";
+import { useRef, useState, useTransition } from "react";
+import { deleteResume } from "./action";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useReactToPrint } from "react-to-print";
 
 interface ResumeItemProps {
-    resume: ResumeServerData
+  resume: ResumeServerData;
 }
 
-export default function ResumeItem({resume}: ResumeItemProps) {
-    const wasUpdated = resume.updateAt !== resume.createdAt;
+export default function ResumeItem({ resume }: ResumeItemProps) {
+  const contentRef = useRef<HTMLDivElement>(null);
 
-    return <div className="group border rounded-lg border-transparent hover:border-border transition-colors bg-secondary">
-        <div className="space-y-3">
-            <Link 
-            href={`/editor?resumeId=${resume.id}`}
-            className="inline-block w-full text-center">
-                <p className="font-semibold line-clamp-1">
-                    {resume.title || "No Title"}
-                </p>
-                {resume.description && (
-                    <p className="line-clamp-2 text-sm">{resume.description}</p>
-                )}
-                <p className="text-xs text-muted-foreground">
-                    {wasUpdated ? "diperbarui" : "dibuat"} tanggal {" "}
-                    {formatDate(resume.updateAt, "MMM d yyyy h:mm a")}
-                </p>
-            </Link>
-            <Link
-            href={`/editor?resumeId=${resume.id}`}
-            className="inline-block w-full"
-            >
-                <ResumePreview
-                resumeData={mapToResumeValues(resume)}
-                className="shadow-md group-hover:shadow-lg transition-shadow"/>
-            </Link>
-        </div>
+  const reactToPrintFn = useReactToPrint({
+    contentRef,
+    documentTitle: resume.title || "CV",
+  });
+
+  const wasUpdated = resume.updateAt !== resume.createdAt;
+
+  return (
+    <div className="group relative rounded-lg border border-transparent bg-secondary transition-colors hover:border-border">
+      <div className="space-y-3">
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="inline-block w-full text-center"
+        >
+          <p className="line-clamp-1 font-semibold">
+            {resume.title || "No Title"}
+          </p>
+          {resume.description && (
+            <p className="line-clamp-2 text-sm">{resume.description}</p>
+          )}
+          <p className="text-xs text-muted-foreground">
+            {wasUpdated ? "diperbarui" : "dibuat"} tanggal{" "}
+            {formatDate(resume.updateAt, "MMM d yyyy h:mm a")}
+          </p>
+        </Link>
+        <Link
+          href={`/editor?resumeId=${resume.id}`}
+          className="relative inline-block w-full"
+        >
+          <ResumePreview
+            resumeData={mapToResumeValues(resume)}
+            contentRef={contentRef}
+            className="overflow-hidden shadow-md transition-shadow group-hover:shadow-lg"   />
+          <div className="absolute inset-x-0 bottom-0 h-1/6 bg-gradient-to-t from-white to-transparent"></div>
+        </Link>
+      </div>
+      <MoreMenu resumeId={resume.id} onPrintClick={reactToPrintFn} />
     </div>
+  );
+}
+
+interface MoreMenuProps {
+  resumeId: string;
+  onPrintClick: () => void;
+}
+
+function MoreMenu({ resumeId, onPrintClick }: MoreMenuProps) {
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            size="icon"
+            className="absolute right-0.5 top-0.5 opacity-0 transition-opacity group-hover:opacity-100"
+          >
+            <MoreVertical className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={() => setShowDeleteConfirmation(true)}
+          >
+            <Trash2 className="size-4" /> Hapus
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="flex items-center gap-2"
+            onClick={onPrintClick}
+          >
+            {" "}
+            <Printer className="size-4" /> Download
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeleteConfirmationDialog
+        resumeId={resumeId}
+        open={showDeleteConfirmation}
+        onOpenChange={setShowDeleteConfirmation}
+      />
+    </>
+  );
+}
+
+interface DeleteConfirmationDialogProps {
+  resumeId: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+function DeleteConfirmationDialog({
+  resumeId,
+  open,
+  onOpenChange,
+}: DeleteConfirmationDialogProps) {
+  const { toast } = useToast();
+
+  const [isPending, startTransition] = useTransition();
+
+  async function handleDelete() {
+    startTransition(async () => {
+      try {
+        await deleteResume(resumeId);
+        onOpenChange(false);
+      } catch (error) {
+        console.error(error);
+        toast({
+          variant: "destructive",
+          description: "Gagal,Coba lagi",
+        });
+      }
+    });
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Hapus CV?</DialogTitle>
+          <DialogDescription>
+            Aksi ini akan menghapus cv secara permanen dan tidak bisa di
+            kembalikan
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button variant="destructive" onClick={handleDelete}>
+            Hapus
+          </Button>
+          <Button variant="secondary" onClick={() => onOpenChange(false)}>
+            Batal
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
