a05e35a5b71df3e9c413f2ca68ffaf08fafcc5d8
Alfiannnn13 <muhammadalfiannovanto17@gmail.com>
2025-02-09 06:57:47 +0700

    updnavf

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -17,103 +17,106 @@ const navLinks = [
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <section className="sticky top-0 z-50 py-4 lg:py-8">
-      <div className="container max-w-5xl">
-        <div className="rounded-[27px] border border-white/15 bg-neutral-950/70 backdrop-blur">
-          <div className="grid grid-cols-2 items-center p-2 px-4 md:pr-2 lg:grid-cols-3">
-            <div>
-              <Image
-                src={logoImage}
-                alt="logo"
-                width={35}
-                height={35}
-                className="h-9 w-auto rounded-sm md:h-auto"
-              />
-            </div>
-            <div className="hidden items-center justify-center lg:flex">
-              <nav className="flex gap-6 font-medium">
-                {navLinks.map((link) => (
-                  <a href={link.href} key={link.label}>
-                    {link.label}
-                  </a>
-                ))}
-              </nav>
-            </div>
-            <div className="flex justify-end">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="feather feather-menu md:hidden"
-                onClick={() => setIsOpen(!isOpen)}
-              >
-                <line
-                  x1="3"
-                  y1="6"
-                  x2="21"
-                  y2="6"
-                  className={twMerge(
-                    "origin-left transition",
-                    isOpen && "-translate-y-1 rotate-45",
-                  )}
-                ></line>
-                <line
-                  x1="3"
-                  y1="12"
-                  x2="21"
-                  y2="12"
-                  className={twMerge("transition", isOpen && "opacity-0")}
-                ></line>
-                <line
-                  x1="3"
-                  y1="18"
-                  x2="21"
-                  y2="18"
-                  className={twMerge(
-                    "origin-left transition",
-                    isOpen && "translate-y-1 -rotate-45",
-                  )}
-                ></line>
-              </svg>
-              <Link href="/resumes">
-                <Button
-                  variant="primary"
-                  className="hidden items-center font-medium md:inline-flex"
-                >
-                  Masuk
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <AnimatePresence>
-            {isOpen && (
-              <motion.div
-                initial={{ height: 0 }}
-                animate={{ height: "auto" }}
-                exit={{ height: 0 }}
-                className="overflow-hidden"
-              >
-                <div className="flex flex-col items-center gap-4 py-4">
+    <>
+      <section className="fixed top-0 z-50 w-full py-4 lg:py-8">
+        <div className="container max-w-5xl">
+          <div className="rounded-[27px] border border-white/15 bg-neutral-950/70 backdrop-blur md:rounded-full">
+            <div className="grid grid-cols-2 items-center p-2 px-4 md:pr-2 lg:grid-cols-3">
+              <div>
+                <Image
+                  src={logoImage}
+                  alt="logo"
+                  width={35}
+                  height={35}
+                  className="h-9 w-auto rounded-sm md:h-auto"
+                />
+              </div>
+              <div className="hidden items-center justify-center lg:flex">
+                <nav className="flex gap-6 font-medium">
                   {navLinks.map((link) => (
-                    <a href={link.href} key={link.label} className="">
+                    <a href={link.href} key={link.label}>
                       {link.label}
                     </a>
                   ))}
-                  <Link href="/resumes">
-                    <Button variant="primary">Masuk</Button>
-                  </Link>
-                </div>
-              </motion.div>
-            )}
-          </AnimatePresence>
+                </nav>
+              </div>
+              <div className="flex justify-end">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="feather feather-menu md:hidden"
+                  onClick={() => setIsOpen(!isOpen)}
+                >
+                  <line
+                    x1="3"
+                    y1="6"
+                    x2="21"
+                    y2="6"
+                    className={twMerge(
+                      "origin-left transition",
+                      isOpen && "-translate-y-1 rotate-45",
+                    )}
+                  ></line>
+                  <line
+                    x1="3"
+                    y1="12"
+                    x2="21"
+                    y2="12"
+                    className={twMerge("transition", isOpen && "opacity-0")}
+                  ></line>
+                  <line
+                    x1="3"
+                    y1="18"
+                    x2="21"
+                    y2="18"
+                    className={twMerge(
+                      "origin-left transition",
+                      isOpen && "translate-y-1 -rotate-45",
+                    )}
+                  ></line>
+                </svg>
+                <Link href="/resumes">
+                  <Button
+                    variant="primary"
+                    className="hidden items-center font-medium md:inline-flex"
+                  >
+                    Masuk
+                  </Button>
+                </Link>
+              </div>
+            </div>
+            <AnimatePresence>
+              {isOpen && (
+                <motion.div
+                  initial={{ height: 0 }}
+                  animate={{ height: "auto" }}
+                  exit={{ height: 0 }}
+                  className="overflow-hidden"
+                >
+                  <div className="flex flex-col items-center gap-4 py-4">
+                    {navLinks.map((link) => (
+                      <a href={link.href} key={link.label} className="">
+                        {link.label}
+                      </a>
+                    ))}
+                    <Link href="/resumes">
+                      <Button variant="primary">Masuk</Button>
+                    </Link>
+                  </div>
+                </motion.div>
+              )}
+            </AnimatePresence>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+      <div className="pb-[86px] md:pb-[98px] lg:px-[130px]"></div>
+    </>
   );
 }
